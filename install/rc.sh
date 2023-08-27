a509bd9807a407f9b1d97231355ec3951afeffb7
Ian Edwards <isedwards@users.noreply.github.com>
2023-08-27 08:54:44 +0100

    Create rc.sh

---
+++ install/rc.sh
@@ -0,0 +1,17 @@
+alias ll=ls -al
+
+t() {
+    if tmux list-sessions &> /dev/null; then
+        tmux attach
+    else
+        tmux
+    fi
+}
+
+vi() {
+    if command -v nvim >/dev/null 2>&1; then
+        nvim "$@"
+    else
+        vim "$@"
+    fi
+}
