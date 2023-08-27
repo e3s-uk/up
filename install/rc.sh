76e8d66320cc20d46c60058f659e558bb4459792
Ian Edwards <isedwards@users.noreply.github.com>
2023-08-27 11:32:45 +0100

    Update rc.sh

--- install/rc.sh
+++ install/rc.sh
@@ -15,3 +15,39 @@ vi() {
         vim "$@"
     fi
 }
+
+mkcd() {
+    if [ "$#" -ne 1 ]; then
+        echo "Usage: mkcd <directory>"
+        return 1
+    fi
+
+    mkdir -p "$1" && cd "$1"
+}
+
+chext() {
+    # Change *.<old-extension> to *.<new-extension>
+    
+    # Check for correct number of arguments
+    if [ "$#" -ne 2 ]; then
+        echo "Usage: chext <old-extension> <new-extension>"
+        return 1
+    fi
+
+    local old_ext="$1"
+    local new_ext="$2"
+
+    # Remove any leading dots from extensions
+    old_ext="${old_ext#.}"
+    new_ext="${new_ext#}"
+
+    for file in *."$old_ext"; do
+        # Check if file exists (in case no files match the old extension)
+        if [ -e "$file" ]; then
+            mv "$file" "${file%.$old_ext}.$new_ext"
+        else
+            echo "No files found with .$old_ext extension."
+            break
+        fi
+    done
+}
