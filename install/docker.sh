863f9120847c1b1fb09547c9a7c3581cdc7e4669
Ian Edwards <isedwards@users.noreply.github.com>
2023-08-31 09:29:15 +0100

    Update docker.sh

--- install/docker.sh
+++ install/docker.sh
@@ -13,7 +13,8 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
-
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
+# https://docs.docker.com/engine/install/linux-postinstall/
+sudo groupadd docker
 sudo usermod -aG docker $USER
